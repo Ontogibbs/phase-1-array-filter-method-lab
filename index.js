91937f8f71d72ce8899396a671ca8015271e53d1
+ <+>
2022-09-20 11:16:31 -0400

    commit tree 72e7b0ce183641c76111120d1e1fb5084e6dbc22

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(arrayOfDriverNames, nameWeAreLookingFor){
+    const result = arrayOfDriverNames.filter(name => name.toUpperCase() === nameWeAreLookingFor.toUpperCase());
+    return (result)
+} 
+ 
+
+function fuzzyMatch(arrayOfDriverNames, nameWeAreLookingFor){
+    const result = arrayOfDriverNames.filter(name => name.charAt(0) === nameWeAreLookingFor.charAt(0));
+    return (result)
+} 
+
+
+function matchName(arrayOfDriverNames, nameWeAreLookingFor){
+    const result = arrayOfDriverNames.filter(query => query.name === nameWeAreLookingFor);
+    return (result)
+} 
+
+
+
+
+
+    
